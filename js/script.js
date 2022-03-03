cd90ad7e904cac82282e83b96c31abd1345e124c
Joao Galhardi <joaogalhardi.dev@gmail.com>
2022-03-03 15:41:36 -0300

    sessao javascript-galeria-plugins-JSdesativado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar Links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link){
@@ -10,3 +11,59 @@ function ativarLink(link){
 
 links.forEach(ativarLink);
 
+// Ativar Items do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  console.log(elemento);
+  if (elemento){
+  elemento.checked = true;
+}
+}
+
+parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event){
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+  
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta){
+  pergunta.addEventListener("click", ativarPergunta)
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas 
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector("bicicleta-imagens");
+
+function trocarImagem(event){
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if(media){
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img){
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+// Animação
+if (window.SimpleAnime){
+  new SimpleAnime();
+}
